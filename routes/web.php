185adfd8dbc5f1dde3e9470d9494ab52508718c9
vanhung130304 <hungvan13032004@gmailcom.comcom>
2025-02-27 06:26:49 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HungController;
 
 Route::get('/', [HungController::class, 'index'])->name('home.index');  // Hiển thị danh sách
-Route::get('/create', [HungController::class, 'create'])->name('home.create');  // Form thêm mới
-Route::post('/store', [HungController::class, 'store'])->name('home.store');  // Lưu dữ liệu mới
+Route::get('/create', [HungController::class, 'create'])->name('home.create'); 
+Route::post('/store', [HungController::class, 'store'])->name('home.store');  
 Route::get('/edit/{id}', [HungController::class, 'edit'])->name('home.edit'); 
 Route::put('/update/{id}', [HungController::class, 'update'])->name('home.update'); 
 Route::delete('/destroy/{id}', [HungController::class, 'destroy'])->name('home.destroy');  
